687f9f104afd41015fdc00de3f5f6d29decdf17a
eimamovic997 <102758643+eimamovic997@users.noreply.github.com>
2022-03-31 20:40:46 +0200

    Add files via upload

---
+++ SET3-Backend/Models/RoleModel.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SET3_Backend.Models
+{
+    public class RoleModel {
+        
+        [Key]
+        public int Id { get; set; }
+        public RoleType RoleType { get; set; }
+
+        public RoleModel() { }
+        public RoleModel(RoleType roleType)
+        {
+            this.RoleType = roleType;
+        }
+    }
+    public enum RoleType
+    {
+        Admin = 0,
+        User = 1
+    }
+}
---
+++ SET3-Backend/Models/UserModel.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SET3_Backend.Models
+{
+    public class UserModel
+    {
+		[Key]
+		public int Id { get; set; }
+		[EmailAddress]
+		public string Email { get; set; }
+		public string FirstName { get; set; }
+		public string LastName { get; set; }
+		public string Password { get; set; }
+		public RoleModel Role { get; set; }
+		public int RoleId { get; set; }
+		public int QuestionId { get; set; }
+		public string Answer { get; set; }
+		public bool Deleted { get; set; }
+
+        public UserModel() { }
+
+        public UserModel(string email, string firstName, string lastName, string password, 
+            RoleModel role, int roleId, int questionId, string answer, bool deleted)
+        {
+            Email = email;
+            FirstName = firstName;
+            LastName = lastName;
+            Password = password;
+            Role = role;
+            RoleId = roleId;
+            QuestionId = questionId;
+            Answer = answer;
+            Deleted = deleted;
+        }
+    }
+}
